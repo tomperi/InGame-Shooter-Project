9ffbde82023dd16acad48066e962b1d16077eafa
Dor <dor.moshe@post.idc.ac.il>
2017-12-23 03:15:10 +0200

    added game controller and enemy scripts

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
     public GameObject Enemy;
+    public GameObject MediumEnemy;
     public float maxSpawnRate;
     public bool startSpawn;
 
-    private float lastEnemyTimeRight, lastEnemyTimeLeft;
+    private float lastEnemyTimeRight, lastEnemyTimeLeft, lastMediumEnemyTimeRight, lastMediumEnemyTimeLeft;
     // Use this for initialization
     void Start () {
+        this.lastMediumEnemyTimeLeft = 0;
+        this.lastMediumEnemyTimeRight = 0;
 	}
 	
 	// Update is called once per frame
@@ -26,6 +29,16 @@ public class GameController : MonoBehaviour {
             {
                 SpawnEnemy(Player.left);
                 lastEnemyTimeLeft = Time.time + Random.Range(0.5f, maxSpawnRate);
+            }
+            if (Time.time >= lastMediumEnemyTimeRight)
+            {
+                SpawnMediumEnemy(Player.right);
+                this.lastMediumEnemyTimeRight = Time.time + 10;
+            }
+            if (Time.time >= lastMediumEnemyTimeLeft)
+            {
+                SpawnMediumEnemy(Player.left);
+                this.lastMediumEnemyTimeLeft = Time.time + 10;
             }
         }
 	}
@@ -43,10 +56,28 @@ public class GameController : MonoBehaviour {
         {
             enemyUnit.transform.position = new Vector2(max.x, Random.Range(min.y, max.y));
             enemyUnit.GetComponent<EnemyControl>().player = Player.right;
-        } else
+        }
+        else
         {
             enemyUnit.transform.position = new Vector2(min.x, Random.Range(min.y, max.y));
             enemyUnit.GetComponent<EnemyControl>().player = Player.left;
+        }
+
+    }
+
+    void SpawnMediumEnemy(Player player)
+    {
+
+        GameObject enemyMediumUnit = Instantiate(MediumEnemy);
+        if (player.Equals(Player.right))
+        {
+            enemyMediumUnit.transform.position = new Vector2(5, Random.Range(-2.5f, 2.5f));
+            enemyMediumUnit.GetComponent<MediumEnemyControl>().player = Player.right;
+        }
+        else
+        {
+            enemyMediumUnit.transform.position = new Vector2(-5, Random.Range(-2.5f, 2.5f));
+            enemyMediumUnit.GetComponent<MediumEnemyControl>().player = Player.left;
         }
 
     }
---
+++ Assets/Scripts/MediumEnemyControl.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediumEnemyControl : MonoBehaviour {
+
+
+    public int MHP;
+    public int EHP;
+    public int direction;
+    public float movementSpeed;
+    public bool down;
+    public Player player;
+
+    public GameObject p_easyenemy;
+
+
+	// Use this for initialization
+	void Start () {
+       
+        Time.timeScale = 1f;
+ //       this.time = Time.time;
+        InvokeRepeating("SpawnEnemy", 1f, 6f);
+        //p_easyenemy.GetComponent<EasyEnemy>().HP = EHP;
+        //p_easyenemy.GetComponent<EasyEnemy>().p_target = p_target;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Move();
+	}
+
+    public void Move()
+    {
+ //       Vector2 move = null;
+        if (down)
+        {
+            
+            if (this.transform.position.y < -2.5f)
+            {
+                down = false;
+                direction = 1;
+//                transform.position = new Vector2(0f, movementSpeed * direction);
+            }
+
+        }
+        else
+        {
+            if (transform.position.y > 2.5f)
+            { 
+                down = true;
+                direction = -1;
+//                transform.position = new Vector3(0f, movementSpeed * direction );
+            }
+        }
+        this.transform.position = new Vector3(transform.position.x, (movementSpeed * direction * Time.deltaTime) + transform.position.y );
+        
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Enemy")
+    //    {
+    //        Physics.IgnoreCollision(other.GetComponent<Collider>(), GetComponent<Collider>());
+    //    }
+
+    //    Debug.Log(other.tag);
+    //    if (other.tag == "bullet") return;
+    //    if (other.tag == "Player")
+    //    {
+    //        //other.GetComponent<PlayerController>().HP -= this.attackDamage;
+    //        Destroy(gameObject);
+    //    }
+    //}
+
+    public void SpawnEnemy()
+    {
+
+        GameObject EasyEnemy = Instantiate(
+         p_easyenemy, this.transform.position, Quaternion.identity);
+        EasyEnemy.GetComponent<EnemyControl>().player = this.player;
+    }
+
+}
